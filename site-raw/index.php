0f3bf77f0acbeedf0ca588d52ae55b7261df681d
Josh Bruce <josh@8fold.pro>
2022-06-26 12:01:37 -0400

    refactor: content method ~50ms

--- site-raw/index.php
+++ site-raw/index.php
@@ -10,37 +10,48 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Eightfold\Markdown\Markdown as MarkdownConverter;
 
-function content() {
+$converterConfig = [
+    'html_input' => 'allow'
+];
+
+$defaultAttributes = [
+    Image::class => [
+        'loading'  => 'lazy',
+        'decoding' => 'async'
+    ]
+];
+
+$externalLinks = [
+    'open_in_new_window' => true,
+    'internal_hosts'     => 'raw.earth.fourth'
+];
+
+$peprmaLinks = [
+    'min_heading_level' => 2,
+    'symbol'            => '＃'
+];
+
+function content(
+    array $config,
+    array $defaultAttributes,
+    array $externalLinks,
+    array $peprmaLinks
+) {
     if (! file_exists('./content.md')) {
         throw new Exception('There is no content for this URI');
     }
 
     return MarkdownConverter::create()
-        ->withConfig(
-            [
-                'html_input' => 'allow'
-            ]
-        )->minified()
+        ->withConfig($config)
+        ->minified()
         ->smartPunctuation()
         ->descriptionLists()
         ->attributes() // for class on notices
-        ->defaultAttributes([
-            Image::class => [
-                'loading'  => 'lazy',
-                'decoding' => 'async'
-            ]
-        ])->abbreviations()
-        ->externalLinks(
-            [
-                'open_in_new_window' => true,
-                'internal_hosts'     => 'joshbruce.com'
-            ]
-        )->accessibleHeadingPermalinks(
-            [
-                'min_heading_level' => 2,
-                'symbol'            => '＃'
-            ],
-        )->convert(
+        ->defaultAttributes($defaultAttributes)
+        ->abbreviations()
+        ->externalLinks($externalLinks)
+        ->accessibleHeadingPermalinks($peprmaLinks)
+        ->convert(
             file_get_contents(__DIR__ . '/content.md')
         );
 }
@@ -55,6 +66,6 @@ function content() {
         <meta name="description" content="A tabletop role playing game for the ages.">
     </head>
     <body>
-        <?php print(content()); ?>
+        <?php print(content($converterConfig, $defaultAttributes, $externalLinks, $peprmaLinks)); ?>
     </body>
 </html>
