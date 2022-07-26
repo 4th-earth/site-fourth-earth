9026e393913e856513b8edb5057caf70f612b6ed
Josh Bruce <josh@8fold.pro>
2022-07-26 18:29:25 -0400

    fix: Navigation

--- src/Templates/Page.php
+++ src/Templates/Page.php
@@ -5,44 +5,32 @@ namespace FE\Templates;
 
 use Eightfold\XMLBuilder\Contracts\Buildable;
 
+use Psr\Http\Message\RequestInterface;
+
 use Eightfold\HTMLBuilder\Document;
 use Eightfold\HTMLBuilder\Element;
 
-use Psr\Http\Message\RequestInterface;
-
 use Eightfold\Amos\Content;
-
+use Eightfold\Amos\Markdown;
 use Eightfold\Amos\PageComponents\PageTitle;
+use Eightfold\Amos\PageComponents\Favicons;
 
-// use Psr\Http\Message\RequestInterface;
-//
-// use Nyholm\Psr7Server\ServerRequestCreator;
-// use Nyholm\Psr7\Factory\Psr17Factory;
-// use Nyholm\Psr7\Response;
-// use Nyholm\Psr7\Stream;
-// use Nyholm\Psr7\Uri;
-//
-// use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
-//
-// use Eightfold\Markdown\Markdown as MarkdownConverter;
-//
-//
-// use FE\Environment;
-// use FE\Components\PageTitle;
-// use FE\Sitemap;
+use FE\PageComponents\Navigation;
 
 class Page implements Buildable
 {
     public static function create(
         Content $contentIn,
-        RequestInterface $request
+        RequestInterface $request,
+        string $withDomain = ''
     ): self {
-        return new self($contentIn, $request);
+        return new self($contentIn, $request, $withDomain);
     }
 
     final private function __construct(
         private Content $contentIn,
-        private RequestInterface $request
+        private RequestInterface $request,
+        private string $withDomain
     ) {
     }
 
@@ -56,6 +44,11 @@ class Page implements Buildable
         return $this->request;
     }
 
+    private function domain(): string
+    {
+        return $this->withDomain;
+    }
+
     public function build(): string
     {
         return Document::create(
@@ -63,45 +56,45 @@ class Page implements Buildable
                     $this->content(),
                     $this->request()
                 )->build()
-                // $this->pageTitle()
             )->head(
-                Element::meta()->props('charset utf-8'),
-                Element::meta()->props(
+                Element::meta()->omitEndTag()->props(
                     'name viewport',
                     'content width=device-width,initial-scale=1'
                 ),
-                Element::meta()->props(
+                Element::meta()->omitEndTag()->props(
                     'name description',
                     'content A tabletop role playing game for the ages.'
                 ),
-                Element::link()->omitEndTag()->props(
-                    'type image/x-icon',
-                    'rel icon',
-                    'href /assets/icons/favicon.ico'
+                Favicons::create(
+                    themeColor: '#ffffff',
+                    path: '/favicons',
+                    msAppTileColor: '#00aba9',
+                    safariTabColor: '#00aba9'
                 ),
-                Element::link()->omitEndTag()->props(
-                    'sizes 180x180',
-                    'rel apple-touch-icon',
-                    'href /assets/icons/apple-touch-icon.png'
-                ),
-                Element::link()->omitEndTag()->props(
-                    'sizes 32x32',
-                    'rel image/png',
-                    'href /assets/icons/favicon-32x32.png'
-                ),
-                Element::link()->omitEndTag()->props(
-                    'sizes 16x16',
-                    'rel image/png',
-                    'href /assets/icons/favicon-16x16.png'
-                ),
-                Element::link()->props('rel stylesheet', 'href /assets/css/styles.min.css'),
-                Element::script()->props('src /assets/js/interactive.js')
+                Element::link()->omitEndTag()
+                    ->props(
+                        'rel stylesheet',
+                        'href /css/styles.min.css',
+                        'type text/css'
+                    ),
+                Element::script()->props(
+                    'src /js/interactive.min.js',
+                    'type text/javascript'
+                )
             )->body(
-                Element::a('Skip to main content')->props('href #main', 'id skip-nav'),
+                Element::a('Skip to main content')
+                    ->props('href #main', 'id skip-nav'),
+                Navigation::create(
+                    $this->content(),
+                    $this->request(),
+                    $this->domain()
+                ),
                 // $this->navigation(),
                 Element::article(
                     Element::section(
-                        // $content
+                        $this->content()->convertedContent(
+                            at: $this->request()->getUri()->getPath()
+                        )
                     )
                 )->props('id main', 'role main'),
                 Element::footer(
@@ -173,323 +166,4 @@ class Page implements Buildable
     {
         return $this->build();
     }
-//     private Psr17Factory $psr17Factory;
-//
-//     private RequestInterface $serverRequest;
-//
-//     private string $path = '';
-//
-//     public static function init(string $site, Environment $environment): Page
-//     {
-//         return new static($site, $environment);
-//     }
-//
-//     public function __construct(
-//         private string $site,
-//         private Environment $environment
-//     ) {
-//         $this->psr17Factory = new Psr17Factory();
-//
-//         $creator = new ServerRequestCreator(
-//             $this->psr17Factory, // ServerRequestFactory
-//             $this->psr17Factory, // UriFactory
-//             $this->psr17Factory, // UploadedFileFactory
-//             $this->psr17Factory  // StreamFactory
-//         );
-//
-//         $this->serverRequest = $creator->fromGlobals();
-//     }
-//
-//     public function serverRequest(): RequestInterface
-//     {
-//         return $this->serverRequest;
-//     }
-//
-//     public function path(): string
-//     {
-//         if (strlen($this->path) === 0) {
-//             $this->path = $this->uri()->getPath();
-//         }
-//         return $this->path;
-//     }
-//
-//     public function authority(): string
-//     {
-//         return $this->uri()->getAuthority();
-//     }
-//
-//     public function response(): Response
-//     {
-//         if ($this->isFile()) {
-//             return $this->fileResponse();
-//         }
-//         return $this->textResponse();
-//     }
-//
-//     private function environment(): Environment
-//     {
-//         return $this->environment;
-//     }
-//
-//     private function psr17Factory(): Psr17Factory
-//     {
-//         return $this->psr17Factory;
-//     }
-//
-//     private function site(): string
-//     {
-//         return $this->site;
-//     }
-//
-//     private function isRoot(): bool
-//     {
-//         return $this->site === 'root';
-//     }
-//
-//     private function isNotRoot(): bool
-//     {
-//         return ! $this->isRoot();
-//     }
-//
-//     private function uri(): Uri
-//     {
-//         return $this->serverRequest()->getUri();
-//     }
-//
-//     private function isFile(): bool
-//     {
-//         return str_contains($this->path(), '.');
-//     }
-//
-//     private function fileResponse(): Response
-//     {
-//         if (
-//             $this->path() === '/vanilla/character-sheet.pdf' or
-//             $this->path() === '/vanilla/social-contract.pdf'
-//         ) {
-//             $resource = @\fopen(__DIR__ . '/../content-raw' . $this->path(), 'r');
-//             if (is_resource($resource)) {
-//                 return new Response(
-//                     status: 200,
-//                     headers: ['content-type' => 'application/pdf'],
-//                     body: Stream::create($resource)
-//                 );
-//             }
-//         }
-//
-//         $resource = @\fopen(__DIR__ . $this->path(), 'r');
-//         $contentType = 'text/plain';
-//         if (str_ends_with($this->path(), '.css')) {
-//             $contentType = 'text/css';
-//
-//         } elseif (str_ends_with($this->path(), '.svg')) {
-//             $contentType = 'image/svg+xml';
-//
-//         } elseif (str_ends_with($this->path(), '.js')) {
-//             $contentType = 'application/javascript';
-//
-//         } elseif (str_ends_with($this->path(), '.ttf')) {
-//             $contentType = 'font/ttf';
-//
-//         } elseif (str_ends_with($this->path(), 'sitemap.xml')) {
-//             $contentType = 'application/xml';
-//             return new Response(
-//                 status: 200,
-//                 headers: ['content-type' => $contentType],
-//                 body: Stream::create(
-//                     Sitemap::create(
-//                         $this->site(),
-//                         $this->environment()->contentRootForSite($this->site()),
-//                         $this->environment()->authorityForSite($this->site()),
-//                         $this->environment()->schemeForSite($this->site())
-//                     )
-//                 )
-//             );
-//         }
-//
-//         if (is_resource($resource)) {
-//             return new Response(
-//                 status: 200,
-//                 headers: ['content-type' => $contentType],
-//                 body: Stream::create($resource)
-//             );
-//         }
-//
-//         return new Response(
-//             status: 404,
-//             headers: ['content-type' => $contentType],
-//             body: 'Content not found.'
-//         );
-//     }
-//
-//     private function contentPath(): string
-//     {
-//         return $this->rootForContent() . $this->path() . 'content.md';
-//     }
-//
-//     private function rootForContent(): string
-//     {
-//         if ($this->shouldUseRootContent()) {
-//             return __DIR__ . '/../content-root';
-//         }
-//         return __DIR__ . '/../content-' . $this->site();
-//     }
-//
-//     private function shouldUseRootContent(): bool
-//     {
-//         return str_starts_with($this->path(), '/legal/') or
-//             str_starts_with($this->path(), '/support/');
-//     }
-//
-//     private function textResponse(): Response
-//     {
-//         $responseBody = $this->psr17Factory()->createStream($this->body());
-//         return $this->psr17Factory()->createResponse(200)->withBody($responseBody);
-//     }
-//
-//     private function body(): string
-//     {
-//         $content = '';
-//         if (
-//             is_string($this->contentPath()) and
-//             file_exists($this->contentPath()) and
-//             $c = file_get_contents($this->contentPath())
-//         ) {
-//             $content = MarkdownConverter::create()
-//                 ->withConfig([
-//                     'html_input' => 'allow'
-//                 ])->defaultAttributes([
-//                     Image::class => [
-//                         'loading'  => 'lazy',
-//                         'decoding' => 'async'
-//                     ]
-//                 ])->externalLinks([
-//                     'open_in_new_window' => true,
-//                     'internal_hosts'     =>
-//                         $this->environment()->authorityForSite($this->site())
-//                 ])->accessibleHeadingPermalinks([
-//                     'min_heading_level' => 2,
-//                     'max_heading_level' => 3,
-//                     'symbol'            => '＃'
-//                 ])->minified()
-//                 ->smartPunctuation()
-//                 ->descriptionLists()
-//                 ->tables()
-//                 ->attributes() // for class on notices
-//                 ->abbreviations()
-//                 ->convert($c);
-//         } else {
-//             $content = <<<html
-//                 <h1>Ready player 404</h1>
-//                 <p>We couldnʼt find that content.</p>
-//                 <p>Maybe it was eaten by a dinosaur. Or perhaps it's just on vacation.</p>
-//             html;
-//
-//             if ($this->site() === 'raw') {
-//                 $content .= <<<html
-//                     <p>Try here instead:</p>
-//                     <ul>
-//                     <li><a href="/">RAW</a></li>
-//                     <li><a href="/vanilla/">Vanilla</a></li>
-//                     <li><a href="/sprinkles/">Sprinkles</a></li>
-//                     </ul>
-//                 html;
-//
-//             } elseif ($this->site() === 'lore') {
-//                 $content .= <<<html
-//                     <p>Try here instead:</p>
-//                     <ul>
-//                     <li><a href="/">Lore</a></li>
-//                     <li><a href="/people/">People</a></li>
-//                     <li><a href="/places/">Places</a></li>
-//                     <li><a href="/things/">Things</a></li>
-//                     </ul>
-//                 html;
-//             }
-//         }
-//
-//     }
-//
-//     private function pageTitle(): string
-//     {
-//         $contentRoot = __DIR__ . '/../content-' . $this->site();
-//         $pageTitle   = PageTitle::init($contentRoot, $this->path());
-//         if ($this->site() === 'raw' or $this->site() === 'lore') {
-//             if (
-//                 str_starts_with($this->path(), '/legal/') or
-//                 str_starts_with($this->path(), '/support/')
-//             ) {
-//                 $part = 'legal';
-//                 if (str_starts_with($this->path(), '/support/')) {
-//                     $part = 'support';
-//                 }
-//                 $baseTitleMeta = $contentRoot . '/meta.json';
-//                 $baseTitle     = PageTitle::titleForPath($baseTitleMeta);
-//
-//                 $contentRoot = __DIR__ . '/../content-root';
-//                 $pageTitle = PageTitle::init($contentRoot, $this->path())
-//                     ->overrideBaseTitle($baseTitle);
-//             }
-//         }
-//         return $pageTitle->title();
-//     }
-//
-//     private function navigation(): Element|string
-//     {
-//         if ($this->isNotRoot()) {
-//             $links = [
-//                 '/ R Rules as Written',
-//                 '/vanilla/ V Vanilla',
-//                 '/sprinkles/ S Sprinkles'
-//             ];
-//             if ($this->site() === 'lore') {
-//                 $links = [
-//                     '/ I Introduction',
-//                     '/people/ Pe People',
-//                     '/places/ Pl Places',
-//                     '/things/ T Things'
-//                 ];
-//             }
-//
-//             $l = [];
-//             $requestPath = $this->path();
-//             foreach ($links as $link) {
-//                 list($href, $ts, $title) = explode(' ', $link, 3);
-//                 $id = str_replace('/', '', $href);
-//
-//                 $a = Element::a(
-//                     ...$this->spans($title, $ts)
-//                 )->props('href ' . $href);
-//                 if ($requestPath === '/' and $href === $requestPath) {
-//                     $a = Element::a(
-//                         ...$this->spans($title, $ts)
-//                     )->props('href ' . $href, 'class current');
-//
-//                 } elseif (
-//                     $href !== '/' and
-//                     str_starts_with($requestPath, $href)
-//                 ) {
-//                     $a = Element::a(
-//                         ...$this->spans($title, $ts)
-//                     )->props('href ' . $href, 'class current');
-//
-//                 }
-//
-//                 $l[] = Element::li($a);
-//             }
-//             return Element::nav(
-//                 Element::ul(...$l)->props('class col-' . count($links))
-//             )->props('is main-nav');
-//
-//         }
-//         return '';
-//     }
-//
-//     private function spans(string $title, string $titleShort): array
-//     {
-//         return [
-//             Element::span($title),
-//             Element::span($titleShort)->props('aria-label ' . $title)
-//         ];
-//     }
 }
