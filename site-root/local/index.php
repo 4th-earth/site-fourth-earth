cbc3d73c9d95924a2a07bea3cc78329bbf53fd14
Josh Bruce <josh@8fold.pro>
2022-07-25 22:00:17 -0400

    incorporate: Amos

--- site-root/local/index.php
+++ site-root/local/index.php
@@ -1,17 +1,18 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('realpath_cache_size', '4096');
+ini_set('realpath_cache_ttl', '600');
+error_reporting(E_ALL);
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
 use FE\Page;
 use FE\Environment;
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-ini_set('realpath_cache_size', '4096');
-ini_set('realpath_cache_ttl', '600');
 
 (new SapiEmitter())->emit(
     Page::init(
