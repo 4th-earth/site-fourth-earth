374c73898175552fada39f6984d09d10ec90a97e
Josh Bruce <josh@8fold.pro>
2026-01-26 09:40:11 -0500

    update: Change toward to to

--- site-root/public/index.php
+++ site-root/public/index.php
@@ -77,7 +77,7 @@ require __DIR__ . '/../../vendor/autoload.php';
         <p>The ages of Earth are not typically known or named except in hindsight.</p>
         <p>First Earth was the rise, time, and fall of humans, also known as homo-sapiens (by them) and First Earthers (by us). They gave the planet its name, which we still use to this day.</p>
         <p>Second Earth was the rise of the <a href="/species/#fenghuang">Fenghuang</a> and decline of humans. The Fenghuang are believed to have created <a href="/places/#the-cradle">The Cradle</a> alongside the humans who remained.</p>
-        <p>Third Earth was the rise and destruction of Maul, and saw many higher-order species migrate to <a href="/places/#the-cradle">The Cradle</a> in waves. Eventually leading Maul toward The Cradle and <a href="/events/#the-battle-over-third-earth">The Battle over Third Earth</a></p>
+        <p>Third Earth was the rise and destruction of Maul, and saw many higher-order species migrate to <a href="/places/#the-cradle">The Cradle</a> in waves. Eventually leading Maul to The Cradle and <a href="/events/#the-battle-over-third-earth">The Battle over Third Earth</a></p>
         <p>Fourth Earth is the time following the destruction of Maul.</p>
     </article>
     <nav id="primary-nav" aria-label="site navigation">
